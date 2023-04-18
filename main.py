2e20980a4ecde12ea010cd0b80f436379cc606f8
SinemYesil <105049945+SinemYesil@users.noreply.github.com>
2023-04-18 11:50:11 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,22 @@
 #q1
 from math import factorial
+
 n = int(input ("Enter a value for n:"));
 x=int (input("Enter the value for x:"));
-
-sit =lambda n, k: n**k/ factorial(k)
-situ=[sit(n,k) for k in range (x+1)]
-
-total= sum(situ)
-total+=1
-
+def calEquation(n,x):
+    sit =lambda n, k: n**k/ factorial(k)
+    situ=[sit(n,k) for k in range (x+1)]
+    total= sum(situ)+1
 
 print("Result:", total )
 #q2
 
 n = int(input("Enter the number for terms: "))
-sum=0
-for k in range (1,n+1):
-    sum += ((-1)**(k+1))/k
+def calSum(n):
+    sum=0
+    for k in range (1,n+1):
+        sum += ((-1)**(k+1))/k
 
 print("The result: ", sum)
 
-#q3
+
